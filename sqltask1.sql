cb14a70bdfa762df9ae48abb961f774f8807a96d
sharmila921 <saaiharsshith921@gmail.com>
2025-12-18 17:30:23 +0530

    Add files via upload

--- sqltask1.sql
+++ sqltask1.sql
@@ -4,3 +4,9 @@ create  table  Patients (PatientID varchar(10), PatientName varchar(50), Age int
 
 alter table patients add column (DoctorAssigned varchar(50));
 
+rename table patients to patients_info;
+insert into patients_info (patientID,patientName,Age,Gender,AdmissionDate,DoctorAssigned) values ('PA101','ragul',32,'Male','22-12-2025','yes');
+
+truncate table patients_info;
+select * from patients_info;
+drop table patients_info;
